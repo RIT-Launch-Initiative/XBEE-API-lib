2ad161f62c4be2b2e2cea66c055f8feadd8dded8
WillMerges <wdmerges@gmail.com>
2021-09-27 19:56:13 -0400

    added initial header file

---
+++ xbee_api.h
@@ -0,0 +1,26 @@
+#ifndef XBEE_API_H
+#define XBEE_API_H
+
+namespace xbee {
+
+    typedef struct {
+        const char* data;
+        size_t size;
+    } api_frame_t;
+
+    typedef enum {
+        vtx_on,  // video transmitter power ON
+        vtx_off, // video transmitter power OFF
+        transmit // transmit frame
+    } packet_t;
+
+    // generate a packet
+    // leave data NULL if vtx cmd
+    api_frame_t generate_packet(packet_t type, const char* data, size_t len);
+
+    // TODO receiving
+    // some kind of parse packet
+}
+
+
+#endif
