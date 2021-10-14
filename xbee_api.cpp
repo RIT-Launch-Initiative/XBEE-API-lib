7c978b6f2a0f048b96ccad398c9a4e51d4168049
WillMerges <wdmerges@gmail.com>
2021-10-14 19:22:34 -0400

    added consts from user manual

--- xbee_api.cpp
+++ xbee_api.cpp
@@ -2,9 +2,20 @@
 
 using namespace xbee;
 
-
+// special bytes (need to be escaped)
 static const uint8_t START_DELIM = 0x7E;
+static const uint8_t ESCAPE      = 0x7D;
+static const uint8_t XON         = 0x11;
+static const uint8_t XOFF        = 0x13;
 
+// needs to be XOR'd with byte being escaped
+static const uint8_t ESCAPE_XOR  = 0x20;
+
+// API Frame ID's
+static const uint8_t AT_CMD      = 0x08; // AT Command
+static const uint8_t AT_CMD_RES  = 0x88; // AT Command Response
+
+// defines an API header
 typedef struct {
     uint8_t start_delim; // start delimeter
     uint16_t length;     // big endian order
